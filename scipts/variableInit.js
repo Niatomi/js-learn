e2de51cd22a9f5c121e9eb1eaf157f22ba553fda
Niatomi <playervoker@gmail.com>
2021-12-22 20:44:23 +0400

    Initial commit

---
+++ scipts/variableInit.js
@@ -0,0 +1,15 @@
+//Переменные
+//variable нетипизированная переменная
+//Не использовать
+var nameExample = "Example";
+
+//Constant константa, readOnly
+//Предпочтительнее использовать такую переменную
+const nameConst = "ConstVar";
+
+//let
+//Хороший тон))
+let age = 26;
+
+//Boolean
+const isSite = true;
