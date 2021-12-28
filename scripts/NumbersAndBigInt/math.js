c75991ef435d92c10aa76085076180f2dd090fd6
Niatomi <playervoker@gmail.com>
2021-12-28 13:04:39 +0400

    Initial commit

---
+++ scripts/NumbersAndBigInt/math.js
@@ -0,0 +1,28 @@
+//Также как и в Java
+
+//Константы
+console.log(Math.E);
+console.log(Math.PI);
+
+//Операции
+console.log(Math.sqrt(4)); //2
+console.log(Math.pow(5, 3)); //125
+console.log(Math.abs(-42)); //42
+console.log(Math.max(42, 100, 12, 1, 12, 123)); //123
+console.log(Math.min(42, 100, 12, 1, 12, 123)); //1
+
+//Округление
+//В меньшую сторону
+console.log(Math.floor(4.6)); //4
+
+//В большую сторону
+console.log(Math.ceil(4.6)); //5
+
+//Ближайщее целое
+console.log(Math.round(4.6)); //5
+
+//Возвращает целую часть числа путём удаления всех дробных знаков
+console.log(Math.trunc(4.6)); //4
+
+//Рандом также как в Java
+console.log(Math.random());
---
+++ scripts/NumbersAndBigInt/mathFloat.js
@@ -0,0 +1,3 @@
+console.log(0.4 + 0.02); //0.420000000000001
+console.log(+(0.4 + 0.02).toFixed(2)); //0.42
+console.log(parseFloat((0.4 + 0.02).toFixed(2))); //0.42
