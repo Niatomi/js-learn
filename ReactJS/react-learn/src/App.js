03d0b0c54177ac8448432de082f00ac16c1d9a70
Niatomi <playervoker@gmail.com>
2022-03-02 17:31:14 +0400

    Add logic to todo controller

--- ReactJS/react-learn/src/App.js
+++ ReactJS/react-learn/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToDoList from "./ToDo/ToDoList";
 
 function App() {
+  let [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "Order bread" },
+    { id: 2, completed: false, title: "Order oil" },
+    { id: 3, completed: false, title: "Order orange " },
+    { id: 4, completed: false, title: "Order something " },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>React Learn</h1>
+      <ToDoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
