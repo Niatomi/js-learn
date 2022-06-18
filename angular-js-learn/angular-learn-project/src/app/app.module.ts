d2910c0c79f3d60dc1b888bde2e50c069f7eb2c6
Niatomi <playervoker@gmail.com>
2022-06-18 14:44:59 +0400

    Initial commit

--- angular-js-learn/angular-learn-project/src/app/app.module.ts
+++ angular-js-learn/angular-learn-project/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponent } from './my/my.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComponent],
+  declarations: [AppComponent, MyComponent, CarsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
