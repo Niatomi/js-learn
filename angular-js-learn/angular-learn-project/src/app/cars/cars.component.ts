beb76bfcb26547c80ae86e5816e0df8485b850af
Niatomi <playervoker@gmail.com>
2022-06-19 23:29:53 +0400

    Add saving names by button

--- angular-js-learn/angular-learn-project/src/app/cars/cars.component.ts
+++ angular-js-learn/angular-learn-project/src/app/cars/cars.component.ts
@@ -9,9 +9,22 @@ export class CarsComponent {
   carName = '';
   inputText = 'Default text';
   addCarStatus = false;
+  cars = ['Audi', 'Volga', 'Land Rover'];
+  items = [
+    { id: 4, name: 'item1' },
+    { id: 5, name: 'item2' },
+    { id: 2, name: 'item3' },
+    { id: 4, name: 'item4' },
+  ];
 
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
+
+    setTimeout(() => {
+      this.addCarStatus = false;
+    }, 1000);
   }
 
   constructor() {}
