3d4cd666333b96d196368ce55cb8db97fb33d462
Niatomi <playervoker@gmail.com>
2022-06-21 01:09:20 +0400

    Add new component

--- angular-js-learn/angular-learn-project/src/app/app.module.ts
+++ angular-js-learn/angular-learn-project/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
 import { FormsModule } from '@angular/forms';
+import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
-  declarations: [AppComponent, CarsComponent, CarComponent],
+  declarations: [AppComponent, CarsComponent, CarComponent, AddCarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
