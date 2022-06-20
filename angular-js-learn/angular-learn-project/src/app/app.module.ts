31a355a2bda6608ed78008ca37cff3e36e1a5ae2
Niatomi <playervoker@gmail.com>
2022-06-20 23:48:59 +0400

    Deleted myComponent from delaretions

--- angular-js-learn/angular-learn-project/src/app/app.module.ts
+++ angular-js-learn/angular-learn-project/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponent } from './my/my.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, MyComponent, CarsComponent, CarComponent],
+  declarations: [AppComponent, CarsComponent, CarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
