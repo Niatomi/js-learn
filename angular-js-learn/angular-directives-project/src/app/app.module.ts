4087153d8b00b82b4c037f4e7272c0d05925c7c1
Niatomi <playervoker@gmail.com>
2022-06-24 00:50:19 +0400

    Add list directive into declarations

--- angular-js-learn/angular-directives-project/src/app/app.module.ts
+++ angular-js-learn/angular-directives-project/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { backgroundDirective } from './directives/background.directive';
+import { hoverDirective } from './directives/hover.dircetive';
 
 @NgModule({
-  declarations: [AppComponent, backgroundDirective],
+  declarations: [AppComponent, backgroundDirective, hoverDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
