f6a12c5414c0bb6c65033d27180f38476d5de2b2
Niatomi <playervoker@gmail.com>
2022-06-24 00:50:41 +0400

    Add input from html

--- angular-js-learn/angular-directives-project/src/app/directives/background.directive.ts
+++ angular-js-learn/angular-directives-project/src/app/directives/background.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2,
 } from '@angular/core';
@@ -13,20 +14,33 @@ import {
 export class backgroundDirective implements OnInit {
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
+  @Input('appBackground')
+  hoverColor: string = 'green';
+
+  @Input()
+  defaultColor: string = 'transparent';
+
   @HostBinding('class')
   usingStyle: string = 'mouse-unhover';
 
-  ngOnInit(): void {}
+  @HostBinding('style.color')
+  textColor: string = 'white';
+
+  ngOnInit(): void {
+    this.textColor = this.defaultColor;
+  }
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    this;
-    this.usingStyle = 'mouse-hover';
+    this.textColor = this.hoverColor;
+    // this.usingStyle = 'mouse-hover';
   }
 
   @HostListener('mouseleave')
   onMouseLeave() {
-    this.usingStyle = 'mouse-unhover';
+    this.textColor = this.defaultColor;
+    // this.renderer.setStyle(this.element, 'color', this.defaultColor);
+    // this.usingStyle = 'mouse-unhover';
     // this.renderer.addClass(this.element.nativeElement, 'white-text');
     // this.element.nativeElement.style.backgroundColor = 'red';
   }
