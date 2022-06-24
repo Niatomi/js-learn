d74f4c7b8641336621400b8fb6328e97397dc75e
Niatomi <playervoker@gmail.com>
2022-06-24 23:40:54 +0400

    Add Import filtering pipe with component

--- angular-js-learn/angular-pipe-learn/src/app/app.module.ts
+++ angular-js-learn/angular-pipe-learn/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CustomPipePipe } from './pipes/custom-pipe.pipe';
+import { FilterPipeComponent } from './filter-pipe/filter-pipe.component';
+import { CarFilterPipe } from './car-filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CustomPipePipe],
+  declarations: [AppComponent, CustomPipePipe, FilterPipeComponent, CarFilterPipe],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
