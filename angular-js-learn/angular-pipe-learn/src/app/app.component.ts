41aee3415637d35805875e3bb436ca42c030839d
Niatomi <playervoker@gmail.com>
2022-06-24 16:45:16 +0400

    Create example pipelines

--- angular-js-learn/angular-pipe-learn/src/app/app.component.ts
+++ angular-js-learn/angular-pipe-learn/src/app/app.component.ts
@@ -2,10 +2,46 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <div class="col-xs-8 col-xs-offset-2"></div> `,
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset-2">
+        <!-- <h2>{{ name }}</h2>
+        <h2>{{ name | lowercase }}</h2>
+        <h2>{{ name | uppercase }}</h2>
+        <h2>{{ name | slice: 0:3 }}</h2>
+        <h2>{{ name | slice: 0:3 | uppercase }}</h2>
+        <hr />
+        <h2>{{ pi }}</h2>
+        <h2>{{ pi | number }}</h2>
+        <h2>{{ pi | number: '1.3-4' }}</h2>
+        <h2>{{ pi | number: '2.2-2' }}</h2>
+        <hr />
+        <h2>{{ money | currency: 'USD':false }}</h2>
+        <h2>{{ money | currency: 'UAH':true }}</h2>
+        <h2>{{ money | currency: 'EUR' }}</h2>
+        <h2>{{ money | currency: 'RUB':true }}</h2> -->
+        <hr />
+        <h2>{{ date }}</h2>
+        <h2>{{ date | date }}</h2>
+        <h2>{{ date | date: 'YYYY' }}</h2>
+        <h2>{{ date | date: 'fullDate' }}</h2>
+        <h2>{{ date | date: 'shortDate' }}</h2>
+        <h2>{{ date | date: 'shortTime' }}</h2>
+        <hr />
+        <h2>{{ amount }}</h2>
+        <h2>{{ amount | percent }}</h2>
+        <h2>{{ amount | percent: '4.3-5' }}</h2>
+        <h2>{{ amount | percent: '4.5' }}</h2>
+        <hr />
+        <h2>
+          <pre>{{ object | json }}</pre>
+        </h2>
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {
+  // https://angular.io/guide/pipes-overview
   name = 'WebForMyself';
   pi = Math.PI;
   money = 350;
