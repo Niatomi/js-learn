5d9b31d1070d576ad0809fd83d453f8b96140e8d
Niatomi <playervoker@gmail.com>
2022-06-24 15:26:36 +0400

    Add data to component

--- angular-js-learn/angular-pipe-learn/src/app/app.component.ts
+++ angular-js-learn/angular-pipe-learn/src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <div class="col-xs-8 col-xs-offset-2"></div> `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-pipe-learn';
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'foo',
+    baz: 'qux',
+    nested: {
+      xyz: 3,
+      number: [1, 2, 3],
+    },
+  };
 }
