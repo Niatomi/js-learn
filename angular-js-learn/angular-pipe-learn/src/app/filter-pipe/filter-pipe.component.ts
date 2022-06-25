38448765f72fb005e58496165e3f943743b05663
Niatomi <playervoker@gmail.com>
2022-06-25 18:29:15 +0400

    Add methods for example of pure parameter of pipe

--- angular-js-learn/angular-pipe-learn/src/app/filter-pipe/filter-pipe.component.ts
+++ angular-js-learn/angular-pipe-learn/src/app/filter-pipe/filter-pipe.component.ts
@@ -2,24 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-filter-pipe',
-  template: ` <input type="text" class="form-control" [(ngModel)]="searchCar" />
+  template: `
+    <hr />
+    <input type="text" class="form-control" [(ngModel)]="searchCar" />
+    <hr />
+
+    <button class="btn btn-primary" (click)="addCar()">Add car</button>
     <hr />
     <ul class="list-group">
       <li
         class="list-group-item"
         *ngFor="
-          let car of cars | carFilter: searchCar:'desc';
+          let car of cars | carFilter: searchCar:'name';
           let index = index
         "
       >
         <b>{{ index + 1 }}</b> {{ car.name }}
         <i> {{ car.desc }}</i>
       </li>
-    </ul>`,
+    </ul>
+  `,
   styleUrls: ['./filter-pipe.component.css'],
 })
 export class FilterPipeComponent implements OnInit {
   searchCar: string = '';
+  carNameInput: string = '';
+  desc: string = '';
 
   cars: { name: string; desc: string }[] = [
     { name: 'Ford', desc: 'Aboba' },
@@ -28,6 +36,13 @@ export class FilterPipeComponent implements OnInit {
     { name: 'BMW', desc: 'Abobus' },
     { name: 'Lamborgini', desc: 'A' },
   ];
+
+  addCar() {
+    this.cars.push({
+      name: 'New Car',
+      desc: 'meow',
+    });
+  }
 
   constructor() {}
 
