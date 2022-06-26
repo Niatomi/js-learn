eb8addc7fb5bc40b2843bbe331c3ab4373cc03ce
Niatomi <playervoker@gmail.com>
2022-06-26 20:50:09 +0400

    Initial commit

---
+++ angular-js-learn/angular-validation-learn/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  answers = [
+    { type: 'yes', text: 'Да' },
+    { type: 'no', text: 'Нет' },
+  ];
+}
