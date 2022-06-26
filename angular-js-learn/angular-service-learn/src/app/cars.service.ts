3e0759fa4dd72595fabcf66031d2bd56c19ef7e2
Niatomi <playervoker@gmail.com>
2022-06-26 14:40:39 +0400

    Initial commit

--- angular-js-learn/angular-service-learn/src/app/cars.service.ts
+++ angular-js-learn/angular-service-learn/src/app/cars.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { ConsoleService } from './console.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarsService {
+  cars: { name: string; isSold: boolean }[] = [
+    {
+      name: 'Ford',
+      isSold: false,
+    },
+    {
+      name: 'BMW',
+      isSold: true,
+    },
+    {
+      name: 'Land Rover',
+      isSold: false,
+    },
+    {
+      name: 'Mazda',
+      isSold: false,
+    },
+  ];
 
-  constructor() { }
+  addCar(carName: string) {
+    this.console.log(carName);
+    this.cars.push({
+      name: carName,
+      isSold: false,
+    });
+  }
+
+  constructor(private console: ConsoleService) {}
 }
