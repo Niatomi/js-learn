09343520a98ad6b45b52559cfae79a88394c8d23
Niatomi <playervoker@gmail.com>
2022-07-15 16:44:07 +0400

    Base project init

--- angular-js-learn/angular-reactive-forms-learn/src/app/app.component.ts
+++ angular-js-learn/angular-reactive-forms-learn/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-reactive-forms-learn';
+  @ViewChild('form') form!: NgForm;
+
+  answers = [
+    { type: 'yes', text: 'Да' },
+    { type: 'no', text: 'Нет' },
+  ];
+
+  defaultAnswer: string = 'yes';
+  defaultCountry: string = 'ua';
+
+  submitForm(form: NgForm) {
+    this.isSubmited = true;
+    this.formData = this.form.value;
+    this.form.reset();
+  }
+
+  formData = {};
+  isSubmited = false;
+
+  addRandomEmail() {
+    const randEmail = 'wfpk@pifam.fef';
+    // this.form.setValue({
+    //   user: {
+    //     email: randEmail,
+    //     password: '',
+    //   },
+    //   country: '',
+    //   answer: '',
+    // });
+    this.form.form.patchValue({
+      user: { email: randEmail },
+    });
+  }
 }
