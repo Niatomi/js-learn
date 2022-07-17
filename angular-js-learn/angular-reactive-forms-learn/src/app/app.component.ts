cf74b0e56ca129bedd48c5d8ef24d9da3a332181
Niatomi <playervoker@gmail.com>
2022-07-17 16:28:14 +0400

    Implemented async validator for email

--- angular-js-learn/angular-reactive-forms-learn/src/app/app.component.ts
+++ angular-js-learn/angular-reactive-forms-learn/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import {
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +15,15 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 export class AppComponent {
   form = new FormGroup({
     user: new FormGroup({
-      email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl('', [Validators.required, this.checkForLength]),
+      email: new FormControl(
+        '',
+        [Validators.email, Validators.required],
+        [<AsyncValidatorFn>this.checkForEmail]
+      ),
+      password: new FormControl('', [
+        Validators.required,
+        this.checkForLength.bind(this),
+      ]),
     }),
     country: new FormControl('ua'),
     answer: new FormControl('no'),
@@ -20,8 +33,14 @@ export class AppComponent {
     return this.form.get('user.password');
   }
 
+  get email() {
+    return this.form.get('user.email');
+  }
+
+  charsCount: number = 5;
+
   checkForLength(control: FormControl) {
-    if (control.value.length <= 4) {
+    if (control.value.length <= this.charsCount) {
       return {
         lengthError: true,
       };
@@ -37,5 +56,19 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.form);
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@mail.ru') {
+          resolve({
+            emailIsUsed: true,
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
   }
 }
