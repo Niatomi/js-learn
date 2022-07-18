989c8ea17d3af51638bf6a25b75a6c13240851ce
Niatomi <playervoker@gmail.com>
2022-07-18 23:29:51 +0400

    Forms module add

--- angular-js-learn/angular-http-learn/src/app/app.module.ts
+++ angular-js-learn/angular-http-learn/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarsService } from './config/cars.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [CarsService],
   bootstrap: [AppComponent],
 })
