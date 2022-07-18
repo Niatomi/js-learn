08aa35b0af1dd365237513435dd7396cb58cd19b
Niatomi <playervoker@gmail.com>
2022-07-18 23:29:40 +0400

    Implemented put and deleting methods

--- angular-js-learn/angular-http-learn/src/app/app.component.ts
+++ angular-js-learn/angular-http-learn/src/app/app.component.ts
@@ -9,6 +9,11 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  colors = ['red', 'green', 'blue', 'pink', 'yellow', 'grey'];
+
+  addCarName = '';
+  addCarColor: string = '';
+
   constructor(private carSerive: CarsService) {}
 
   ngOnInit(): void {
@@ -17,7 +22,35 @@ export class AppComponent implements OnInit {
 
   cars: Car[] = [];
 
+  setNewColor(id: number) {
+    this.cars[id - 1].color = this.getRandomColor();
+    this.carSerive.changeColor(this.cars[id - 1]).subscribe(() => {});
+  }
+
+  getRandomColor() {
+    const num = Math.round(Math.random() * (this.colors.length - 1));
+    return this.colors[num];
+  }
+
+  deleteCar(id: number) {
+    this.carSerive.deleteCar(id).subscribe(() => {
+      this.getCars();
+    });
+  }
+
   getCars() {
     this.carSerive.getCars().subscribe((cars: Car[]) => (this.cars = cars));
+  }
+
+  submitCar() {
+    this.carSerive
+      .addCar({
+        id: 0,
+        name: this.addCarName,
+        color: this.addCarColor,
+      })
+      .subscribe((car) => {
+        this.cars.push(car);
+      });
   }
 }
--- angular-js-learn/angular-http-learn/src/app/config/cars.service.ts
+++ angular-js-learn/angular-http-learn/src/app/config/cars.service.ts
@@ -11,8 +11,27 @@ export interface Car {
 @Injectable()
 export class CarsService {
   constructor(private http: HttpClient) {}
-
+  baseURL = 'http://localhost:3000/cars';
   getCars(): Observable<any> {
-    return this.http.get<Car[]>('http://localhost:3000/cars');
+    return this.http.get<Car[]>(this.baseURL);
+  }
+
+  addCar(car: Car): Observable<any> {
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify(car);
+    console.log(body);
+    return this.http.post<Car>(this.baseURL, body, { headers: headers });
+  }
+
+  getCar(id: number): Observable<any> {
+    return this.http.get<Car>(this.baseURL + `/${id}`);
+  }
+
+  changeColor(car: Car) {
+    return this.http.put<Car>(this.baseURL + `/${car.id}`, car);
+  }
+
+  deleteCar(id: number) {
+    return this.http.delete(this.baseURL + `/${id}`);
   }
 }
