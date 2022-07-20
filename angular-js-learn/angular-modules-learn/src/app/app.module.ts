ac742d2006ffb51a05b55a3ff1883f8ce6dba32b
Niatomi <playervoker@gmail.com>
2022-07-20 21:29:50 +0400

    Navbar moved to its own file

--- angular-js-learn/angular-modules-learn/src/app/app.module.ts
+++ angular-js-learn/angular-modules-learn/src/app/app.module.ts
@@ -5,20 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { CarsPageComponent } from './cars-page/cars-page.component';
 import { FormsModule } from '@angular/forms';
-import { CarPageComponent } from './car-page/car-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ColorDirective } from './shared/color.directive';
+import { HeaderComponent } from './header/header.component';
+import { CarsModule } from './cars-page/car.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    CarsPageComponent,
-    CarPageComponent,
     NotFoundComponent,
+    ColorDirective,
+    HeaderComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CarsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
