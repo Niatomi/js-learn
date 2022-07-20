e80746100aa4af3ee085a9f9b83408331e0a65b7
Niatomi <playervoker@gmail.com>
2022-07-20 17:31:02 +0400

    Routes moved to their own module

--- angular-js-learn/angular-routes-learn/src/app/app-routing.module.ts
+++ angular-js-learn/angular-routes-learn/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarPageComponent } from './car-page/car-page.component';
+import { CarsPageComponent } from './cars-page/cars-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cars', component: CarsPageComponent },
+  { path: 'cars/:id', component: CarPageComponent },
+  { path: 'cars/:id/:name', component: CarPageComponent },
+  { path: '', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-js-learn/angular-routes-learn/src/app/app.module.ts
+++ angular-js-learn/angular-routes-learn/src/app/app.module.ts
@@ -7,22 +7,11 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { FormsModule } from '@angular/forms';
-import { Routes, Router, RouterModule } from '@angular/router';
-
-const appRoutes: Routes = [
-  { path: 'cars', component: CarsPageComponent },
-  { path: '', component: HomePageComponent },
-];
+import { CarPageComponent } from './car-page/car-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, CarsPageComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-  ],
+  declarations: [AppComponent, HomePageComponent, CarsPageComponent, CarPageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
