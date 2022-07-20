a8945328e5c10acb5d30072909e519237c7c6e94
Niatomi <playervoker@gmail.com>
2022-07-20 21:32:14 +0400

    Moved to car's folder

---
+++ angular-js-learn/angular-modules-learn/src/app/cars-page/car-page/car-page.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Car, CarsServiceService } from 'src/app/cars-service.service';
+
+@Component({
+  selector: 'app-car-page',
+  templateUrl: './car-page.component.html',
+  styleUrls: ['./car-page.component.css'],
+})
+export class CarPageComponent implements OnInit {
+  id: string = '';
+  carById: Car = this.service.getCarById(1);
+  carByName: any;
+  name: string = '';
+
+  color: string = '';
+  year: number = -1;
+  hash: string | null = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: CarsServiceService
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.name = this.route.snapshot.params['name'];
+
+    this.color = this.route.snapshot.queryParams['color'];
+    this.year = this.route.snapshot.queryParams['year'];
+    this.hash = this.route.snapshot.fragment;
+
+    this.carById = this.service.getCarById(+this.id);
+    this.carByName = this.service.getCarByName(this.name);
+
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.carById = this.service.getCarById(parseInt(this.id));
+    });
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.color = params['color'];
+      this.year = params['year'];
+    });
+  }
+
+  getBackToHomePage() {
+    this.router.navigate(['cars'], { relativeTo: this.route.parent });
+  }
+}
