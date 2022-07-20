f6d2a3a72c213bd7c556d0ab23b166fddb6ec5bc
Niatomi <playervoker@gmail.com>
2022-07-20 17:31:55 +0400

    Add some params for car clars object and add getters

--- angular-js-learn/angular-routes-learn/src/app/cars-service.service.ts
+++ angular-js-learn/angular-routes-learn/src/app/cars-service.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 export interface Car {
   name: string;
   id: number;
+  year: number;
+  color: string;
 }
 
 @Injectable({
@@ -13,18 +15,49 @@ export class CarsServiceService {
     {
       name: 'Lada',
       id: 1,
+      year: 2001,
+      color: 'red',
     },
     {
       name: 'UAZ',
       id: 2,
+      year: 2003,
+      color: 'white',
     },
     {
       name: 'Mercedes',
       id: 3,
+      year: 2005,
+      color: 'blue',
     },
     {
       name: 'Audi',
       id: 4,
+      year: 2006,
+      color: 'yellow',
     },
   ];
+
+  emptyCar: Car = {
+    name: 'Not found',
+    id: -1,
+    year: -1,
+    color: 'Not found',
+  };
+
+  getCarById(id: number) {
+    var returningCar: Car = this.emptyCar;
+    this.cars.forEach((car) => {
+      if (car.id === id) returningCar = car;
+    });
+    return returningCar;
+  }
+
+  getCarByName(name: string) {
+    var returningCar: Car = this.emptyCar;
+    this.cars.forEach((car) => {
+      if (car.name === name) returningCar = car;
+    });
+    return returningCar;
+  }
 }
