d6d29876691d34b5a70af445b1dace256fa5a69d
Niatomi <playervoker@gmail.com>
2022-07-20 17:30:04 +0400

    Add button for open cars page

--- angular-js-learn/angular-routes-learn/src/app/home-page/home-page.component.ts
+++ angular-js-learn/angular-routes-learn/src/app/home-page/home-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openCarsPage() {
+    this.router.navigate(['cars'], { relativeTo: this.route });
   }
-
 }
