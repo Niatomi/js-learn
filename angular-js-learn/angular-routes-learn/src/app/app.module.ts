b70cbcd1675415d2fa088ee2528373193a5596dc
Niatomi <playervoker@gmail.com>
2022-07-20 19:02:56 +0400

    Add not found component to imports

--- angular-js-learn/angular-routes-learn/src/app/app.module.ts
+++ angular-js-learn/angular-routes-learn/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { FormsModule } from '@angular/forms';
 import { CarPageComponent } from './car-page/car-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, CarsPageComponent, CarPageComponent],
+  declarations: [AppComponent, HomePageComponent, CarsPageComponent, CarPageComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
