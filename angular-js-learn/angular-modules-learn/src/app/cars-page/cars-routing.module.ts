ce9679e1ab2aae0599f45a45d5bf82f5604fb97d
Niatomi <playervoker@gmail.com>
2022-07-20 21:28:40 +0400

    Cars routes moved to their own file

---
+++ angular-js-learn/angular-modules-learn/src/app/cars-page/cars-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CarPageComponent } from './car-page/car-page.component';
+import { CarsPageComponent } from './cars-page.component';
+
+const carsRoutes: Routes = [
+  {
+    path: 'cars',
+    component: CarsPageComponent,
+    children: [
+      { path: ':id', component: CarPageComponent },
+      { path: ':id/:name', component: CarPageComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(carsRoutes)],
+  exports: [RouterModule],
+})
+export class CarsRoutingModule {}
