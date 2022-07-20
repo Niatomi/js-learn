84d6a44868f3b8559ac1cd170972d28495fc9189
Niatomi <playervoker@gmail.com>
2022-07-20 19:00:58 +0400

    Add children components and error handler

--- angular-js-learn/angular-routes-learn/src/app/app-routing.module.ts
+++ angular-js-learn/angular-routes-learn/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarPageComponent } from './car-page/car-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'cars', component: CarsPageComponent },
-  { path: 'cars/:id', component: CarPageComponent },
-  { path: 'cars/:id/:name', component: CarPageComponent },
+  {
+    path: 'cars',
+    component: CarsPageComponent,
+    children: [
+      { path: ':id', component: CarPageComponent },
+      { path: ':id/:name', component: CarPageComponent },
+    ],
+  },
   { path: '', component: HomePageComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' },
 ];
 
 @NgModule({
