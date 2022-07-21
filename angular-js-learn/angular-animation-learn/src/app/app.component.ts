8f7b5f7bf3cd17cac1e4233b8d5ae8426869e6d7
Niatomi <playervoker@gmail.com>
2022-07-21 17:23:18 +0400

    Created animation

--- angular-js-learn/angular-animation-learn/src/app/app.component.ts
+++ angular-js-learn/angular-animation-learn/src/app/app.component.ts
@@ -1,10 +1,57 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedDiv', [
+      state(
+        'start',
+        style({
+          backgroundColor: 'blue',
+          width: '150px',
+          height: '150px',
+        })
+      ),
+      state(
+        'end',
+        style({
+          backgroundColor: 'red',
+          width: '300px',
+          height: '300px',
+        })
+      ),
+      state(
+        'active',
+        style({
+          width: '170px',
+          height: '170px',
+          backgroundColor: 'yellow',
+        })
+      ),
+
+      transition('start <=> end', animate(300)),
+      transition('start => active', animate(300)),
+      transition('active => end', animate(400)),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'angular-animation-learn';
+  clickedDivState: string = 'start';
+
+  changeDivState() {
+    this.clickedDivState = 'end';
+    setTimeout(() => {
+      this.clickedDivState = 'start';
+    }, 3000);
+  }
 }
