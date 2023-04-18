4ba167f1b31707e7eaeba191cebbd5e9fee35d62
Niatomi <playervoker@gmail.com>
2023-04-19 01:42:13 +0400

    Initial commit

--- vue-learn-project/src/components/AddTodo.vue
+++ vue-learn-project/src/components/AddTodo.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>Todo form</h1>
+    <h1>Todo add</h1>
+    <div class="input-div">
+        <form @submit.prevent="onSubmit">
+            <input type="text" v-model="title"/>
+            <button class="btn1">Submit</button>
+        </form>
+    </div>
 </template>>
+
+<script>
+export default {
+    data() { 
+        return {
+            title: ''
+        }
+    },
+    name: 'AddTodo',
+    props: ['data'],
+    methods: {
+        onSubmit(data) {
+            console.log(this.title)
+        }
+
+    },
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input {
+        width: 400px;
+    }
+</style>>
